2a3810c8de442c2980dba32926d6cf058199181a
summerbird-1 <2718075060@qq.com>
2022-03-27 14:56:32 +0800

    struct.h

--- structs.h
+++ structs.h
@@ -11,10 +11,10 @@ struct Trade                      //每条交易记录的结构体
 {
 	int ID;                      //交易号码
 	char* Ttime;                   //交易时间
-	float Number[10];           //该笔交易各类商品交易数，下标对应商品序号
-	float Amount[10];          //该笔交易各类商品交易额，下标代表商品序号
-	float Receive;          //收到金额
-	float Change;              //找零金额
+	float Number[10];            //该笔交易各类商品交易数，下标对应商品序号
+	float Amount[10];            //该笔交易各类商品交易额，下标代表商品序号
+	float Receive;               //收到金额
+	float Change;                //找零金额
 	float DiscountAmount;        //会员优惠金额
 	int CustomerId;            //用户账号
 	struct Trade* next;
